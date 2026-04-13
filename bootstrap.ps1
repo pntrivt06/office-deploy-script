a8ecbe9dc1a563bd37f2b2be6b6d8e11dd86738a
Tri Phan <pntri.vt06@outlook.com>
2026-04-13 14:04:02 +0700

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,7 @@
-$u="https://raw.githubusercontent.com/pntrivt06/office-deploy-script/main/Deploy.ps1"
-$f="$env:TEMP\Deploy.ps1"
-iwr $u -OutFile $f
+$u  = "https://raw.githubusercontent.com/<USERNAME>/office-deploy-script/main/office-menu.ps1"
+$f  = "$env:TEMP\office-menu.ps1"
+
+Invoke-WebRequest $u -OutFile $f
+
 powershell -ExecutionPolicy Bypass -File $f
+``
