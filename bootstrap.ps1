8aa3b9b9e5ceb2dc95a1b90830d412d81835778a
Tri Phan <pntri.vt06@outlook.com>
2026-04-13 09:20:04 +0700

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
-$u="https://raw.githubusercontent.com/USERNAME/office-deploy-script/main/Deploy.ps1"
+$u="https://raw.githubusercontent.com/pntrivt06/office-deploy-script/main/Deploy.ps1"
 $f="$env:TEMP\Deploy.ps1"
 iwr $u -OutFile $f
 powershell -ExecutionPolicy Bypass -File $f
