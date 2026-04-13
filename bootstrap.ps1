c10a37fde561f20dfb7d1e248ce410f37ccfcc26
Tri Phan <pntri.vt06@outlook.com>
2026-04-13 14:11:37 +0700

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,5 @@
-$u  = "https://raw.githubusercontent.com/pntrivt06/office-deploy-script/main/office-menu.ps1"
-$f  = "$env:TEMP\office-menu.ps1"
+$u = "https://raw.githubusercontent.com/pntrivt06/office-deploy-script/main/office-menu.ps1"
+$f = "$env:TEMP\office-menu.ps1"
 
 Invoke-WebRequest $u -OutFile $f
 
